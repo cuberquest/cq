1d38df9353e768550e7d3e5c14645d1c05b342b4
cuberquest <134105195+cuberquest@users.noreply.github.com>
2024-10-16 15:49:11 -0400

    v1.0.2.1 - adding scripts for modules

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
 import Game from "./classes/game.js";
 
 let canvas = document.body.insertBefore(document.createElement("canvas"), document.getElementById("gs"));
-let game = new Game(canvas);
+const game = new Game(canvas);
