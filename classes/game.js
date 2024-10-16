0bb4b3cc4a78991b9630496d3706ba6b1dd275ea
cuberquest <134105195+cuberquest@users.noreply.github.com>
2024-10-16 15:18:25 -0400

    v1.0.2.0 - fixing error on imported `.js` files

--- classes/game.js
+++ classes/game.js
@@ -1,4 +1,4 @@
-import Player from "./player";
+import Player from "./player.js";
 
 export default class Game {
   constructor(canvas) {
--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import Game from "./classes/game";
+import Game from "./classes/game.js";
 
 let canvas = document.body.insertBefore(document.createElement("canvas"), document.getElementById("gs"));
 let game = new Game(canvas);
