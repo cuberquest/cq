2bf9d4a758c2cfe37111a2d1c95e367a2b7591af
cuberquest <134105195+cuberquest@users.noreply.github.com>
2024-11-07 14:19:19 -0500

    v1.0.3.6 - various site fixes

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ addEventListener("keydown", key);
 if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
   document.body.classList.remove("light");
   document.body.classList.add("dark");
-} else if (window.matchMedia & window.matchMedia("(prefers-color-scheme: light)")) {
+} else if (window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches) {
   document.body.classList.remove("dark");
 }
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
